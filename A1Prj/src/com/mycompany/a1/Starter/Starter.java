3f9cde8afe99813dd681b80bd648ca5c3510cda6
Varun Ved <varunmved@gmail.com>
2016-02-16 12:38:09 -0800

    trying to use subfolders

---
+++ A1Prj/src/com/mycompany/a1/Starter/Starter.java
@@ -0,0 +1,41 @@
+package com.mycompany.a1.Starter;
+
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import java.io.IOException;
+
+/**
+ * Created by Varun on 2/16/16.
+ */
+public class Starter {
+    private Form current;
+
+    public void init(Object context) {
+        try{
+            Resources theme = Resources.openLayered("/theme");
+            UIManager.getInstance().setThemeProps(theme.getTheme(theme.getThemeResourceNames()[0]));
+       }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        Form hi = new Form("Hi World");
+        hi.addComponent(new Label("Hi World"));
+        hi.show();
+    }
+
+    public void stop() {
+        current = Display.getInstance().getCurrent();
+    }
+
+    public void destroy() {
+    }
+}
